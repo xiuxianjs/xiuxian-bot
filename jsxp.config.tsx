372a24870643e3debd94abe87a25a8cafd1b7d2c
ningmengchongshui <ningmengchongshui@gmail.com>
2024-12-17 18:07:32 +0800

    update: 升级

--- jsxp.config.tsx
+++ jsxp.config.tsx
@@ -18,96 +18,96 @@ import event from '@src/assets/defset/user.json'
 
 export default defineConfig({
   routes: {
-    '/AssMessage': {
-      // body 内容
-      component: (
-        <Component.AssMessage
-          theme={'purple'}
-          data={await user_ass
-            .findAll({
-              where: {
-                uid: event.UserId
-              },
-              include: [
-                {
-                  model: ass,
-                  include: [
-                    {
-                      model: ass_typing
-                    }
-                  ]
-                }
-              ]
-            })
-            .then(res => res.map(item => item?.dataValues))}
-        />
-      )
-    },
-    '/BagComponent': {
-      component: (
-        <Component.BagComponent
-          theme={'purple'}
-          data={await backpackInformation(
-            event.UserId,
-            event.UserAvatar,
-            Goods.mapType['道具']
-          )}
-        />
-      )
-    },
-    '/Defsetcomponent': {
-      component: <Component.Defsetcomponent theme={'dark'} data={Cooling} />
-    },
-    '/Equipmentcomponent': {
-      // body 内容
-      component: (
-        <Component.Equipmentcomponent
-          theme={'purple'}
-          data={await equipmentInformation(event.UserId, event.UserAvatar)}
-        />
-      )
-    },
     '/HelpComponent': {
       // body 内容
       component: (
         <Component.HelpComponent theme={'dark'} data={json_base_help} />
       )
-    },
-    '/UpdateComponent': {
-      component: <Component.UpdateComponent theme={'dark'} data={update_josn} />
-    },
-    '/SkillsComponent': {
-      component: (
-        <Component.SkillsComponent
-          theme={'dark'}
-          data={await skillInformation(event.UserId, event.UserAvatar)}
-        />
-      )
-    },
-    '/KillComponent': {
-      // body 内容
-      component: (
-        <Component.KillComponent theme={'dark'} data={await getKillList()} />
-      )
-    },
-    '/MessageComponent': {
-      component: (
-        <Component.MessageComponent
-          theme={'purple'}
-          data={await personalInformation(event.UserId, event.UserAvatar)}
-        />
-      )
-    },
-    '/SkyComponent': {
-      component: (
-        <Component.SkyComponent
-          theme={'dark'}
-          data={await showSky(event.UserId)}
-        />
-      )
-    },
-    '/Activety': {
-      component: <Component.Activety theme={'dark'} select="limit_time" />
     }
+    // '/AssMessage': {
+    //   // body 内容
+    //   component: (
+    //     <Component.AssMessage
+    //       theme={'purple'}
+    //       data={await user_ass
+    //         .findAll({
+    //           where: {
+    //             uid: event.UserId
+    //           },
+    //           include: [
+    //             {
+    //               model: ass,
+    //               include: [
+    //                 {
+    //                   model: ass_typing
+    //                 }
+    //               ]
+    //             }
+    //           ]
+    //         })
+    //         .then(res => res.map(item => item?.dataValues))}
+    //     />
+    //   )
+    // },
+    // '/BagComponent': {
+    //   component: (
+    //     <Component.BagComponent
+    //       theme={'purple'}
+    //       data={await backpackInformation(
+    //         event.UserId,
+    //         event.UserAvatar,
+    //         Goods.mapType['道具']
+    //       )}
+    //     />
+    //   )
+    // },
+    // '/Defsetcomponent': {
+    //   component: <Component.Defsetcomponent theme={'dark'} data={Cooling} />
+    // },
+    // '/Equipmentcomponent': {
+    //   // body 内容
+    //   component: (
+    //     <Component.Equipmentcomponent
+    //       theme={'purple'}
+    //       data={await equipmentInformation(event.UserId, event.UserAvatar)}
+    //     />
+    //   )
+    // },
+    // '/UpdateComponent': {
+    //   component: <Component.UpdateComponent theme={'dark'} data={update_josn} />
+    // },
+    // '/SkillsComponent': {
+    //   component: (
+    //     <Component.SkillsComponent
+    //       theme={'dark'}
+    //       data={await skillInformation(event.UserId, event.UserAvatar)}
+    //     />
+    //   )
+    // },
+    // '/KillComponent': {
+    //   // body 内容
+    //   component: (
+    //     <Component.KillComponent theme={'dark'} data={await getKillList()} />
+    //   )
+    // },
+    // '/MessageComponent': {
+    //   component: (
+    //     <Component.MessageComponent
+    //       theme={'purple'}
+    //       data={await personalInformation(event.UserId, event.UserAvatar)}
+    //     />
+    //   )
+    // },
+    // '/SkyComponent': {
+    //   component: (
+    //     <Component.SkyComponent
+    //       theme={'dark'}
+    //       data={await showSky(event.UserId)}
+    //     />
+    //   )
+    // },
+    // '/Activety': {
+    //   component: <Component.Activety theme={'dark'} select="limit_time" />
+    // }
   }
 })
