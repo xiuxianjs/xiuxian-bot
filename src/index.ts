60b912fed89e5bd5345bef1c0f99655c3d11d5bf
ningmengchongshui <ningmengchongshui@gmail.com>
2024-12-29 18:39:40 +0800

    feat: 更新文档

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { defineChildren } from 'alemonjs'
-export default defineChildren(() => {
-  return {
-    async onCreated() {
-      console.info('修仙机器人启动')
-      await import('./sql')
-    }
+export default defineChildren(() => ({
+  async onCreated() {
+    console.info('修仙机器人启动')
+    await import('./sql')
   }
-})
+}))
