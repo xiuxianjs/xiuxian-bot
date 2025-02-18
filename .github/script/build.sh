23fb63422126d6c9eb8e1adf7d277f51dc7f5fe1
xiejihuang <xiejihuang@baidu.com>
2025-02-19 00:13:57 +0800

    update: sh

--- .github/script/build.sh
+++ .github/script/build.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Creating distribution directory..."
 mkdir -p dist
+# 复制.npmrc文件
 mv .npmrc dist/
 
 echo "Installing dependencies and building the project..."
@@ -19,3 +20,6 @@ cp -rf ./packages/xiuxian/lib dist/
 cp -rf ./packages/xiuxian/.puppeteerrc.cjs dist/
 cp -rf ./packages/xiuxian/package.json dist/
 cp -rf ./packages/xiuxian/pm2.config.cjs dist/
+
+# 恢复.npmrc文件
+cp dist/.npmrc ./.npmrc
